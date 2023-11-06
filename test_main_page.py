41396a0ef9bf0327f3c2db7f8d4b4f8d8944866b
RossKh <ross.kharriman@gmail.com>
2023-11-06 13:27:25 +0100

    Page Object to test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,7 @@
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
-import time
-link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+from .pages.main_page import MainPage
 
-
-def test_guest_should_see_button(browser):
-    browser.get(link)
-    try:
-        button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-        assert button is not None, "Кнопка не найдена на странице"
-    except NoSuchElementException:
-        assert False, "Кнопка не найдена на странице"
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
